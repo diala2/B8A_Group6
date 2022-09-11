425b580de02c03b2261668238ecb98089e102c21
diala2 <112942959+diala2@users.noreply.github.com>
2022-09-11 22:01:27 +0300

    Update Main.java

--- src/pkg251project/Main.java
+++ src/pkg251project/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-          System.out.println("hello");
+          System.out.println("hello,I'm Diala");
           System.out.println("hello, I'm Dalaa");
         
     }
